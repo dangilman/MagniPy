1aaab17c5289afc3778f6a14e59b7b7cdaee8f3e
Daniel Gilman <gilmanda@ucla.edu>
2018-08-28 12:40:19 -0400

    update

---
+++ MagniPy/LensBuild/lens_system.py
@@ -0,0 +1,72 @@
+from MagniPy.util import polar_to_cart
+import numpy as np
+
+class LensSystem:
+
+    """
+    This class is a single, complete lens model with a main deflector and halos in the lens plane and along LOS.
+    """
+
+    def __init__(self,main_deflector,realization=None,multiplane=False):
+
+        self.lens_components = []
+        self.redshift_list = []
+        self.multiplane = multiplane
+
+        self.main_lens(main_deflector)
+        self.realization = realization
+
+    def _build(self):
+
+        if not hasattr(self, '_halo_names'):
+            if self.realization is not None:
+                self._halo_names, self._halo_redshifts, self._halo_kwargs = self.realization.lensing_quantities()
+            else:
+                self._halo_names, self._halo_redshifts, self._halo_kwargs = [], [], []
+
+        main_names, main_redshift, main_args = self._unpack_main(self.main)
+
+        lens_model_names = main_names + self._halo_names
+        lens_model_redshifts = np.append(main_redshift, self._halo_redshifts)
+        lens_model_kwargs = main_args + self._halo_kwargs
+
+        return lens_model_names, lens_model_redshifts, lens_model_kwargs
+
+    def _unpack_main(self, main):
+
+        if main is None:
+            return [], [], []
+
+        if main.parameterization == 'SIE_shear':
+
+            zlist = [self.zmain]*2
+            names = ['SPEMD','SHEAR']
+            shear_e1, shear_e2 = polar_to_cart(main.shear, main.shear_theta)
+            kwargs = [main.lenstronomy_args]
+            kwargs.append({'e1': shear_e1, 'e2': shear_e2})
+
+        if main.parameterization == 'SERSIC_NFW':
+
+            zlist = [self.zmain] * 3
+            names = ['SERSIC_ELLIPSE', 'SHEAR', 'NFW']
+            shear_e1, shear_e2 = polar_to_cart(main.shear, main.shear_theta)
+            kwargs = main.lenstronomy_args['SERSIC']
+            kwargs.append({'e1': shear_e1, 'e2': shear_e2})
+            kwargs.append(main.lenstronomy_args['NFW'])
+
+        return names, zlist, kwargs
+
+    def main_lens(self, deflector_main):
+
+        if deflector_main is None:
+            self.main = None
+            return
+        self.lens_components += [deflector_main]
+        self.main = deflector_main
+        self.zmain = deflector_main.redshift
+
+    def lenstronomy_lists(self):
+
+        lens_list, zlist, arg_list = self._build()
+
+        return zlist,lens_list,arg_list
---
+++ MagniPy/LensBuild/main_deflector.py
@@ -0,0 +1,131 @@
+from MagniPy.MassModels.ExternalShear import Shear
+from MagniPy.Solver.GravlensWrap.kwargs_translate import model_translate_togravlens
+from MagniPy.Solver.LenstronomyWrap.kwargs_translate import model_translate_tolenstronomy
+from MagniPy.util import polar_to_cart, cart_to_polar
+
+
+class Deflector(object):
+
+    def __init__(self, subclass=classmethod, redshift=float,
+                 tovary=False, varyflags = None, **lens_kwargs):
+
+        self.tovary = tovary
+
+        self.subclass = subclass
+
+        args, self.other_args = subclass.params(**lens_kwargs)
+
+        self.lenstronomy_args = model_translate_tolenstronomy(args, name=self.other_args['name'])
+        self.gravlens_args = model_translate_togravlens(self.lenstronomy_args, name=self.other_args['name'])
+        self.parameterization = self.other_args['parameterization']
+
+        self.redshift = redshift
+
+        self.lensing = subclass
+
+        if self.tovary:
+            self.varyflags = varyflags
+        else:
+            self.varyflags = ['0'] * 10
+
+        if 'shear' in lens_kwargs:
+            assert 'shear_theta' in lens_kwargs
+            self.has_shear = True
+            self.shear_theta = lens_kwargs['shear_theta']
+            self.set_shear(lens_kwargs['shear'])
+
+        else:
+            self.has_shear = False
+
+        if self.has_shear:
+
+            self.Shear = Shear()
+
+        else:
+
+            self.shear_args = {'e1':1e-10,'e2':1e-10}
+
+    def update_lenstronomy_args(self,newargs):
+
+        for key in newargs.keys():
+            self.lenstronomy_args[key] = newargs[key]
+
+        self.gravlens_args = model_translate_togravlens(self.lenstronomy_args, name=self.other_args['name'])
+
+    def set_varyflags(self,flags):
+
+        self.varyflags = flags
+
+    def print_args(self):
+
+        for item in self.args:
+            print(item+': '+str(self.args[item]))
+        for item in self.other_args:
+            print(item+': '+str(self.other_args[item]))
+        if self.has_shear:
+            print('shear: ',self.shear)
+            print('shear PA: ',self.shear_theta)
+
+    def set_shear(self,newshear):
+
+        self.shear = newshear
+        e1,e2 = polar_to_cart(self.shear,self.shear_theta)
+        self.shear_args = {'e1':e1,'e2':e2}
+
+    def update(self,method=None,is_shear=False,**newparams):
+
+        assert method in ['lensmodel','lenstronomy']
+
+        if method=='lenstronomy':
+
+            if is_shear:
+
+                s, spa = cart_to_polar(newparams['e1'], newparams['e2'])
+
+                self.shear_theta = spa
+
+                self.set_shear(s)
+
+                return
+
+            if self.parameterization == 'SERSIC_NFW':
+
+                if 'theta_Rs' in newparams.keys():
+                    self.lenstronomy_args['NFW'].update(newparams)
+                else:
+                    self.lenstronomy_args['SERSIC'].update(newparams)
+
+            elif self.parameterization == 'SERSIC_NFW_DISK':
+
+                if 'theta_Rs' in newparams.keys():
+
+                    self.lenstronomy_args['NFW'].update(newparams)
+
+                elif 'n_sersic' in newparams.keys():
+
+                    self.lenstronomy_args['SERSIC'].update(newparams)
+
+            else:
+
+                self.lenstronomy_args.update(newparams)
+                self.gravlens_args = model_translate_togravlens(newparams,self.other_args['name'])
+
+        elif method=='lensmodel':
+
+            self.lenstronomy_args = model_translate_tolenstronomy(newparams,name=self.other_args['name'])
+            self.gravlens_args = model_translate_togravlens(self.lenstronomy_args,self.other_args['name'])
+
+            self.shear = newparams['shear']
+            self.shear_theta = newparams['shear_theta']
+
+            self.set_shear(self.shear)
+
+    def translate_model(self,to='lenstronomy'):
+
+        if to == 'lenstronomy':
+
+            return model_translate_tolenstronomy(self.args,name=self.other_args['name'])
+
+        if to == 'lensmodel':
+
+            return model_translate_togravlens(self.lenstronomy_args, name=self.other_args['name'])
