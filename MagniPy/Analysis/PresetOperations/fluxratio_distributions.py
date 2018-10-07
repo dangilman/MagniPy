cc8f3825f95c4464a5c22903396ae5e5459813d2
Daniel Gilman <gilmanda@ucla.edu>
2018-10-07 10:04:15 -0700

    update

--- MagniPy/Analysis/PresetOperations/fluxratio_distributions.py
+++ MagniPy/Analysis/PresetOperations/fluxratio_distributions.py
@@ -14,7 +14,7 @@ def compute_fluxratio_distributions(halo_model='', model_args={},
                                     filter_halo_positions=False, outfilepath=None, ray_trace=True, method='lenstronomy',
                                     start_shear=0.05, mindis_front=0.5, mindis_back=0.3, logmcut_back=None, logmcut_front=None,
                                     single_background=False,n_restart=1, pso_conv_mean = 100,
-                                    srcx = 0, srcy = 0, use_source=True, hierarchical = True):
+                                    srcx = 0, srcy = 0, use_source=True, hierarchical = True, grid_res = 0.002):
     tstart = time()
     data = Data(x=data2fit[0],y=data2fit[1],m=data2fit[2],t=data2fit[3],source=[srcx, srcy])
 
@@ -83,24 +83,24 @@ def compute_fluxratio_distributions(halo_model='', model_args={},
             if hierarchical:
                 model_data, system, _ = solver.hierarchical_optimization(datatofit=data, macromodel=start_macromodel,
                                                                        realizations=use_real,
-                                                                       multiplane=multiplane, n_particles=50,
+                                                                       multiplane=multiplane, n_particles=20,
                                                                        simplex_n_iter=400, n_iterations=300,
                                                                        source_size_kpc=source_size_kpc,
                                                                        optimize_routine='fixed_powerlaw_shear',
                                                                        verbose=True, pso_convergence_mean=pso_conv_mean,
                                                                        re_optimize=False, particle_swarm=True,
-                                                                       restart=n_restart)
+                                                                       restart=n_restart, grid_res=grid_res)
 
             else:
                 model_data, system = solver.optimize_4imgs_lenstronomy(datatofit=data, macromodel=start_macromodel,
                                                                        realizations=use_real,
-                                                                       multiplane=multiplane, n_particles=50,
+                                                                       multiplane=multiplane, n_particles=20,
                                                                        simplex_n_iter=400, n_iterations=300,
                                                                        source_size_kpc=source_size_kpc,
                                                                        optimize_routine='fixed_powerlaw_shear',
                                                                        verbose=True, pso_convergence_mean=pso_conv_mean,
                                                                        re_optimize=False, particle_swarm=True,
-                                                                       restart=n_restart)
+                                                                       restart=n_restart, grid_res=grid_res)
 
             for sys,dset in zip(system,model_data):
 
