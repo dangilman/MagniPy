8608377ca31a83be23d096fcd03e0832e205c1e7
Daniel Gilman <gilmanda@ucla.edu>
2018-10-07 13:42:10 -0700

    update

--- MagniPy/Solver/solveroutines.py
+++ MagniPy/Solver/solveroutines.py
@@ -21,39 +21,44 @@ class SolveRoutines(Magnipy):
                                   simplex_n_iter=300, background_globalmin_masses = None,
                                   background_aperture_masses = None, background_filters = None,
                                   mindis_front = 0.5, logmasscut_front = 7.5, min_mass = 6, m_break = 0,
-                                  particle_swarm_reopt = True, save_all_paths=True):
+                                  particle_swarm_reopt = True, save_all_paths=True, reoptimize_scale = None):
 
         if background_globalmin_masses is None or background_aperture_masses is None:
             m_ref = max(m_break, min_mass)
+
+            background_aperture_masses = [10]
+            background_globalmin_masses = [10]
+            background_filters = [1]
+            reoptimize_scale = [1]
+            particle_swarm_reopt = [True]
+
             if m_ref < 7:
-                background_aperture_masses = [10, 8, 7.4, 0]
-                background_globalmin_masses = [10, 9, 8, 8]
-                background_filters = [0.5, 0.5, 0.2, 0.03]
-                reoptimize_scale = [1, 1, 0.3, 0.2]
-                particle_swarm_reopt = [True, True, False, False]
+                background_aperture_masses += [8.5, 7.5, 0]
+                background_globalmin_masses += [8.5, 8, 8]
+                background_filters += [0.5, 0.15, 0.01]
+                reoptimize_scale += [1, 0.3, 0.2]
+                particle_swarm_reopt += [True, False, False]
 
             elif m_ref < 7.5:
-                background_aperture_masses = [10, 7.5, 0]
-                background_globalmin_masses = [10, 8, 8]
-                background_filters = [0.5, 0.4, 0.03]
-                reoptimize_scale = [1, 0.2, 0.2]
-                particle_swarm_reopt = [True, True, False, False]
+                background_aperture_masses += [7.5, 0]
+                background_globalmin_masses += [8, 8]
+                background_filters += [0.4, 0.03]
+                reoptimize_scale += [0.2, 0.2]
+                particle_swarm_reopt += [True, False, False]
 
             elif m_ref < 8:
-                background_aperture_masses = [9, 8, 0]
-                background_globalmin_masses = [9, 8, 8]
-                background_filters = [0.5, 0.5, 0.025]
-                reoptimize_scale = [1, 1, 0.2]
-                particle_swarm_reopt = [True, True, False]
+                background_aperture_masses += [8, 0]
+                background_globalmin_masses += [8, 8]
+                background_filters += [0.5, 0.025]
+                reoptimize_scale += [1, 0.2]
+                particle_swarm_reopt += [True, False]
 
             else:
-                background_aperture_masses = [9, 0]
-                background_globalmin_masses = [9, 8]
-                background_filters = [0.5, 0.3]
-                reoptimize_scale = [1, 0.2]
-                particle_swarm_reopt = [True, False]
-        else:
-            reoptimize_scale = np.ones_like(background_filters)
+                background_aperture_masses += [0]
+                background_globalmin_masses += [8]
+                background_filters += [0.3]
+                reoptimize_scale += [0.2]
+                particle_swarm_reopt += [False]
 
         if not isinstance(particle_swarm_reopt, list):
             particle_swarm_reopt = [particle_swarm_reopt] * len(background_filters)
