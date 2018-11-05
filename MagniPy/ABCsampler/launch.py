7c3e1376db02e7936c66ca07de2bfc24fdce4120
Daniel Gilman <gilmanda@ucla.edu>
2018-11-05 11:54:06 -0800

    launch script

--- MagniPy/ABCsampler/launch.py
+++ MagniPy/ABCsampler/launch.py
@@ -8,7 +8,7 @@ job_index = int(sys.argv[1])
 
 # edit the chain_ID to specify the folder containing the 'paramdictionary' files for each simulation.
 # the param dictionary files should be in a folder located in the 'prefix' directory, see "paths.py"
-chain_ID = 'WDM_run_7.7'
+chain_ID = 'test_routine'
 
 #time.sleep(200)
 runABC(prefix+chain_ID+'/',job_index)
