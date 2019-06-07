5c5cff4fcf145b5fdb97349917e35dd5a4b8cc07
Daniel Gilman <gilmanda@ucla.edu>
2019-06-07 14:25:07 -0700

    update

--- MagniPy/ABCsampler/ABCsampler.py
+++ MagniPy/ABCsampler/ABCsampler.py
@@ -59,7 +59,7 @@ def run_lenstronomy(data, prior, keys, keys_to_vary, halo_constructor, solver,
     N_computed = 0
     init_macro = False
     t0 = time.time()
-    readout_steps = 2
+    readout_steps = 50
     verbose = True
 
     current_best = 1e+6
