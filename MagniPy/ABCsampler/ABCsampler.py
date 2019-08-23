52dcf937b93a28a77faf9e8f72e1205b61a746b3
Daniel Gilman <gilmanda@ucla.edu>
2019-08-23 11:27:21 -0700

    update

--- MagniPy/ABCsampler/ABCsampler.py
+++ MagniPy/ABCsampler/ABCsampler.py
@@ -341,6 +341,6 @@ def write_info_file(fpath,keys,keys_to_vary,pnames_vary):
 #cpl = 2000
 #L = 21
 #index = (L-1)*cpl + 1
-#runABC(prefix+'data/lens0911_mcrelation/', 1)
+#runABC(prefix+'data/lens0405_varymlow_test/', 1)
 
 
